22d878a6c8177296e08b00a4db7a625eeb560673
Kyle Collie <kcollie@hotmail.com>
2019-01-21 17:13:46 -0500

    finished section "C"

--- public/script.js
+++ public/script.js
@@ -1,6 +1,16 @@
 new Vue({
 	el: "#app",
 	data: {
-		total: 0
+		total: 0,
+		products: [
+			{ title: "Product1", id: 1 },
+			{ title: "Product2", id: 2 },
+			{ title: "Product3", id: 3 }
+		]
+	},
+	methods: {
+		addToCart: function () {
+			this.total += 9.99;
+		}
 	}
 });
